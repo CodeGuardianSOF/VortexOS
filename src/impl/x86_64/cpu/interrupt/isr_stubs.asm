052bff6337f31b85c0906ccfda25af8c98af115a
Michael Petch <mpetch@gmail.com>
2024-10-15 16:03:09 -0600

    Fix some exceptions I missed in last update that the CPU pushes an error code for. Clean up the stub macros to reduce repeated code.

--- src/impl/x86_64/cpu/interrupt/isr_stubs.asm
+++ src/impl/x86_64/cpu/interrupt/isr_stubs.asm
@@ -49,35 +49,18 @@ extern handle_interrupt
     pop rax
 %endmacro
 
-%macro isr_stub_error 1
+; ISR stub generation macro
+; 1st Argument is the ISR number
+; 2nd Argument optional error code to push
+%macro isr_stub 1-2
 global isr%1_stub
 extern isr%1
 
 isr%1_stub:
-    push %1                    ; Push the ISR number on stack
-    save_registers
-
-    mov eax, 0x10              ; Load data selectors with kernel data selector
-    mov ds, eax
-    mov es, eax
-    mov fs, eax
-    mov gs, eax
-
-    mov rdi, rsp
-    cld
-    call handle_interrupt
-
-    restore_registers
-    add rsp, 16                ; Remove interrupt numbeer and error
-    iretq
-%endmacro
-
-%macro isr_stub_noerror 1
-global isr%1_stub
-extern isr%1
-
-isr%1_stub:
-    push 0                     ; Push dummy error code 0
+    ; Push a dummy error code if a 2nd parameter has been passed to the macro
+%if %0 == 2
+    push %2                    ; Push dummy error code 0
+%endif
     push %1                    ; Push the ISR number on stack
     save_registers
 
@@ -97,53 +80,55 @@ isr%1_stub:
 %endmacro
 
 ; Define stubs for exceptions (interrupts 0-31)
-isr_stub_noerror 0             ; Divide by Zero
-isr_stub_noerror 1             ; Debug Exception
-isr_stub_noerror 2             ; NMI Interrupt
-isr_stub_noerror 3             ; Breakpoint
-isr_stub_noerror 4             ; Overflow
-isr_stub_noerror 5             ; Out of Bounds
-isr_stub_noerror 6             ; Invalid Opcode
-isr_stub_noerror 7             ; Device Not Available
-isr_stub_error   8             ; Double Fault
-isr_stub_noerror 9             ; Coprocessor Segment Overrun
-isr_stub_error   10            ; Invalid TSS
-isr_stub_error   11            ; Segment Not Present
-isr_stub_error   12            ; Stack Fault
-isr_stub_error   13            ; General Protection Fault
-isr_stub_error   14            ; Page Fault
-isr_stub_noerror 15            ; Reserved
-isr_stub_noerror 16            ; x87 Floating Point Exception
-isr_stub_noerror 17            ; Alignment Check
-isr_stub_noerror 18            ; Machine Check
-isr_stub_noerror 19            ; SIMD Floating Point Exception
-isr_stub_noerror 20            ; Reserved
-isr_stub_noerror 21            ; Reserved
-isr_stub_noerror 22            ; Reserved
-isr_stub_noerror 23            ; Reserved
-isr_stub_noerror 24            ; Reserved
-isr_stub_noerror 25            ; Reserved
-isr_stub_noerror 26            ; Reserved
-isr_stub_noerror 27            ; Reserved
-isr_stub_noerror 28            ; Reserved
-isr_stub_noerror 29            ; Reserved
-isr_stub_noerror 30            ; Security Exception
-isr_stub_noerror 31            ; Reserved
+; Second parameter to isr_stub is the dummy error code (0) to push
+;     for interrupts that the CPU doesn't push an error code for
+isr_stub 0, 0                  ; Divide by Zero
+isr_stub 1, 0                  ; Debug Exception
+isr_stub 2, 0                  ; NMI Interrupt
+isr_stub 3, 0                  ; Breakpoint
+isr_stub 4, 0                  ; Overflow
+isr_stub 5, 0                  ; Out of Bounds
+isr_stub 6, 0                  ; Invalid Opcode
+isr_stub 7, 0                  ; Device Not Available
+isr_stub 8                     ; Double Fault
+isr_stub 9, 0                  ; Coprocessor Segment Overrun
+isr_stub 10                    ; Invalid TSS
+isr_stub 11                    ; Segment Not Present
+isr_stub 12                    ; Stack Fault
+isr_stub 13                    ; General Protection Fault
+isr_stub 14                    ; Page Fault
+isr_stub 15, 0                 ; Reserved
+isr_stub 16, 0                 ; x87 Floating Point Exception
+isr_stub 17                    ; Alignment Check
+isr_stub 18, 0                 ; Machine Check
+isr_stub 19, 0                 ; SIMD Floating Point Exception
+isr_stub 20, 0                 ; Reserved
+isr_stub 21                    ; Reserved
+isr_stub 22, 0                 ; Reserved
+isr_stub 23, 0                 ; Reserved
+isr_stub 24, 0                 ; Reserved
+isr_stub 25, 0                 ; Reserved
+isr_stub 26, 0                 ; Reserved
+isr_stub 27, 0                 ; Reserved
+isr_stub 28, 0                 ; Reserved
+isr_stub 29                    ; Reserved
+isr_stub 30                    ; Security Exception
+isr_stub 31, 0                 ; Reserved
 
 ; Define stubs for interrupt handlers (IRQ0-IRQ15)
-isr_stub_noerror 32
-isr_stub_noerror 33
-isr_stub_noerror 34
-isr_stub_noerror 35
-isr_stub_noerror 36
-isr_stub_noerror 37
-isr_stub_noerror 38
-isr_stub_noerror 39
-isr_stub_noerror 40
-isr_stub_noerror 41
-isr_stub_noerror 42
-isr_stub_noerror 43
-isr_stub_noerror 44
-isr_stub_noerror 45
-isr_stub_noerror 46
-isr_stub_noerror 47
+isr_stub 32, 0
+isr_stub 33, 0
+isr_stub 34, 0
+isr_stub 35, 0
+isr_stub 36, 0
+isr_stub 37, 0
+isr_stub 38, 0
+isr_stub 39, 0
+isr_stub 40, 0
+isr_stub 41, 0
+isr_stub 42, 0
+isr_stub 43, 0
+isr_stub 44, 0
+isr_stub 45, 0
+isr_stub 46, 0
+isr_stub 47, 0
